79b4ac66bce63b2a24ee86a34cfca39bb7d48415
frida <frida.blomster@ha.ax>
2024-04-29 10:14:10 +0300

    Knight

--- src/test/java/ax/ha/tdd/chess/engine/KnightTests.java
+++ src/test/java/ax/ha/tdd/chess/engine/KnightTests.java
@@ -2,11 +2,11 @@ package ax.ha.tdd.chess.engine;
 
 import ax.ha.tdd.chess.console.ChessboardWriter;
 import ax.ha.tdd.chess.engine.pieces.Knight;
+import ax.ha.tdd.chess.engine.pieces.Pawn;
 import ax.ha.tdd.chess.engine.pieces.Rook;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertIterableEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class KnightTests {
 
@@ -44,6 +44,23 @@ public class KnightTests {
         chessboard.addPiece(knight);
         Rook rook = new Rook(Color.BLACK, new Square("f3"));
         chessboard.addPiece(rook);
+
+        //Assert
+        assertTrue(knight.canMove(chessboard, new Square("f3")));
+
+        System.out.println(new ChessboardWriter().print(chessboard));
+    }
+
+    @Test
+    public void testCanKnightJumpObstacleShouldBeLegal() {
+        //Arrange
+        Chessboard chessboard = new ChessboardImpl();
+        Knight knight = new Knight(Color.WHITE, new Square("g1"));
+        chessboard.addPiece(knight);
+        Rook rook = new Rook(Color.BLACK, new Square("f3"));
+        chessboard.addPiece(rook);
+        Pawn pawn = new Pawn(Color.WHITE, new Square("g2"));
+        chessboard.addPiece(pawn);
 
         //Assert
         assertTrue(knight.canMove(chessboard, new Square("f3")));
